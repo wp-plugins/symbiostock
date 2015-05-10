50343227e1f8cd45ba137207f12e27782e818cd5
Symbiostock <Symbiostock@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-10 08:37:28 +0000

    Adding 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/symbiostock/tags/1.0.1@1157191 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin-global.php
@@ -0,0 +1,100 @@
+<?php
+
+$currSS->disable_direct_access();
+
+// Remove excess menu pages
+
+add_action( 'admin_menu', 'remove_taxonomy_menu_pages');
+function remove_taxonomy_menu_pages() {
+	if (!$GLOBALS['currSS']->simplify()) return;
+//	register_taxonomy('product_tag','woocommerce_taxonomy_objects_product_tag', array('show_ui' => false));
+}
+
+add_action( 'admin_menu', 'ss_remove_submenu_excess', 999 );
+function ss_remove_submenu_excess() {
+	if (!$GLOBALS['currSS']->simplify()) return;
+	$page = remove_submenu_page('edit.php?post_type=product', 'edit-tags.php?taxonomy=product_shipping_class&post_type=product' );
+	$page = remove_submenu_page('edit.php?post_type=product', 'product_attributes' );
+//  $page = remove_submenu_page('edit.php?post_type=product', 'product_tags' );
+//  $page = add_submenu_page('edit.php?post_type=product', __( 'Licenses', 'ss' ), __( 'Licenses', 'ss' ), 'manage_product_terms', 'edit-tags.php?taxonomy=pa_license&post_type=product' );
+}
+
+//Symbiostock Branding
+
+// Change titles
+add_filter('admin_title', 'ss_admin_title', 10, 2);
+function ss_admin_title($admin_title, $title) {
+	$admin_title = str_replace('WooCommerce','Symbiostock',$admin_title);
+    return $admin_title;
+}
+
+add_filter( 'admin_footer_text', 'ss_footer_text',999);
+function ss_footer_text($footer_text) {
+	if (isset($GLOBALS['currSS']->issspage) || stristr($footer_text,'woocommerce')) return 'Thank you for selling with Symbiostock and WooCommerce. Please leave us a <a href="https://wordpress.org/support/view/plugin-reviews/symbiostock#postform" target="_blank">&#9733;&#9733;&#9733;&#9733;&#9733;</a> rating if you find Symbiostock useful!';
+	return $footer_text;
+}
+
+add_filter('woocommerce_register_post_type_product', 'ss_product_to_media');
+function ss_product_to_media($arr) {
+	$arr['labels']['name'] = __('Media','ss');
+	$arr['labels']['singular_name'] = __('Media','ss');
+	$arr['labels']['menu_name'] = __('Symbiostock','ss');
+	$arr['labels']['add_new'] = __('Add Media','ss');
+	$arr['labels']['add_new_item'] = __('Add New Media','ss');
+	$arr['labels']['edit_item'] = __('Edit Media','ss');
+	$arr['labels']['new_item'] = __('New Media','ss');
+	$arr['labels']['view'] = __('View Media','ss');
+	$arr['labels']['view_item'] = __('View Media','ss');
+	$arr['labels']['search_items'] = __('Search Media','ss');
+	$arr['labels']['not_found'] = __('No Media found','ss');
+	$arr['labels']['not_found_in_trash'] = __('No Media found in trash','ss');
+	$arr['labels']['parent'] = __('Parent Media','ss');
+	return $arr;
+}
+
+add_filter('admin_menu', 'ss_change_menutitle',999);
+function ss_change_menutitle($title) {
+	if (!$GLOBALS['currSS']->simplify()) return;
+	global $menu,$submenu;
+
+	foreach ($menu as $key => $value) {
+		if ($value[0] == 'Dashboard') $dashkey = $key;
+		if ($value[0] == 'Symbiostock') $sskey = $key;
+		if ($value[0] == 'WooCommerce') $wookey = $key;
+	}
+	$seperator = array("","read","separator1","","wp-menu-separator");
+	if (isset($dashkey)) $newmenu[2] = $menu[$dashkey];
+	if (isset($sskey)) $newmenu[4] = $menu[$sskey];
+	if (isset($wookey)) $newmenu[3] = $menu[$wookey];
+	if (isset($dashkey)) unset($menu[$dashkey]);
+	if (isset($sskey)) unset($menu[$sskey]);
+	if (isset($dashkey)) unset($menu[$wookey]);
+	$menu = array_merge($newmenu,$menu);
+
+	foreach ($submenu["edit.php?post_type=product"] as $key => $value) {
+		if ($value[0] == 'Symbiostock') $submenu["edit.php?post_type=product"][$key][0] = 'Media';
+		if ($value[0] == 'Tags') {
+			$submenu["edit.php?post_type=product"][$key][0] = 'Licenses';
+			$submenu["edit.php?post_type=product"][$key][2] = 'edit-tags.php?taxonomy=pa_license&post_type=product';
+		}
+	}
+}
+
+	add_action( 'admin_head', 'ss_admin_head_icon');
+		function ss_admin_head_icon() {
+//#adminmenu #toplevel_page_woocommerce .menu-icon-generic div.wp-menu-image:before
+?>
+<style type="text/css">
+#adminmenu #menu-posts-product .menu-icon-post div.wp-menu-image:before {
+	background-image: url('<?php print $GLOBALS['currSS']->ss_web_assets_dir; ?>ss_ico.png');
+    background-size: 20px 20px;
+	background-repeat:no-repeat;
+	background-position: center;
+	width: 20px; height: 20px;
+    content:"";
+}
+</style>
+<?php
+		}
+
+?>
---
+++ admin-settings.php
@@ -0,0 +1,208 @@
+<?php
+
+$currSS->disable_direct_access();
+
+ss_create_Settings::init();
+class ss_create_Settings {
+    public static function init() {
+		add_action( 'admin_menu',  __CLASS__ . '::add_ss_settings_page' );
+    }
+
+	function add_ss_settings_page() {
+		add_submenu_page('edit.php?post_type=product', 'Symbiostock Settings', 'Settings', 'manage_product_terms', 'manage_ss_settings', __CLASS__ .'::ss_settings_page');
+	}
+
+	function ss_settings_page() {
+		$GLOBALS['currSS']->issspage = 1;
+
+		if(isset($_POST['fpass'])) {
+			do_action('ss_start_options_receive');
+
+			if (isset($_POST['ss_download_limit'])) update_option('ss_download_limit',sanitize_text_field($_POST['ss_download_limit']));
+			if (isset($_POST['ss_download_expiry'])) update_option('ss_download_expiry',sanitize_text_field($_POST['ss_download_expiry']));
+			if (isset($_POST['ss_watermarkpercent'])) update_option('ss_watermarkpercent',sanitize_text_field($_POST['ss_watermarkpercent']));
+			update_option('ss_woo_product_page',sanitize_text_field($_POST['ss_woo_product_page']));
+			update_option('ss_simplify_interface',sanitize_text_field($_POST['ss_simplify_interface']));
+			update_option('ss_auto_publish',sanitize_text_field($_POST['ss_auto_publish']));
+			update_option('ss_pingback',sanitize_text_field($_POST['ss_pingback']));
+
+			if (isset($_POST['ss_reset_watermark'])) {
+				copy($GLOBALS['currSS']->ss_default_watermark_loc, $GLOBALS['currSS']->ss_watermark_loc);
+			}
+			if (is_uploaded_file($_FILES["ss_new_watermark"]["tmp_name"])) {
+				if ((ss_get_mime_type($_FILES["ss_new_watermark"]["tmp_name"]) == 'image/png') && (filesize($_FILES["ss_new_watermark"]["tmp_name"])< 200000)) {
+					move_uploaded_file($_FILES["ss_new_watermark"]["tmp_name"], $GLOBALS['currSS']->ss_watermark_loc);
+				} else {
+					unlink($_FILES["ss_new_watermark"]["tmp_name"]);
+					$err=1;
+				}
+			}
+
+			do_action('ss_end_options_receive');
+		}
+?>
+<?php if (isset($err)) { ?>
+		<div class="error">
+			<p><?php print __('Sorry, that file did not register as a PNG or was too large in size.','ss'); ?></p>
+		</div>
+<?php } ?>
+<div class="wrap woocommerce">
+	<form method="post" id="mainform" action="" enctype="multipart/form-data">
+<input type="hidden" name="fpass" value="1">
+		<h3>Symbiostock Settings</h3><table class="form-table">
+
+<tr valign="top">
+						<th scope="row" class="titledesc">
+							<label for="ss_download_limit">Download Limit</label>
+									</th>
+						<td class="forminp forminp-number">
+							<input
+								name="ss_download_limit"
+								id="ss_download_limit"
+								type="number"
+								style=""
+								value="<?php print get_option('ss_download_limit'); ?>"
+								class=""
+								placeholder=""
+																/> <span class="description">Number of times media can be downloaded once purchased. Leave blank for unlimited.</span>						</td>
+					</tr><tr valign="top">
+						<th scope="row" class="titledesc">
+							<label for="ss_download_expiry">Download Expiry</label>
+													</th>
+						<td class="forminp forminp-number">
+							<input
+								name="ss_download_expiry"
+								id="ss_download_expiry"
+								type="number"
+								style=""
+								value="<?php print get_option('ss_download_expiry'); ?>"
+								class=""
+								placeholder=""
+																/> <span class="description">Number of days media is available for downloaded once purchased. Leave blank for unlimited.</span>						</td>
+					</tr><tr valign="top">
+						<th scope="row" class="titledesc">
+							<label for="ss_simplify_interface">Personalize Interface</label>
+													</th>
+						<td class="forminp forminp-number">
+							<input
+								name="ss_simplify_interface"
+								id="ss_simplify_interface"
+								type="checkbox"
+								style=""
+								value="1"
+								class=""
+								placeholder=""
+								<?php if (get_option('ss_simplify_interface')) print 'checked'; ?>
+																/> <span class="description">Simplify administrative interface to focus on media sales. <b>(Recommended)</b></span>						</td>
+					</tr><tr valign="top">
+						<th scope="row" class="titledesc">
+							<label for="ss_woo_product_page">Personalize Product Page</label>
+													</th>
+						<td class="forminp forminp-number">
+							<input
+								name="ss_woo_product_page"
+								id="ss_woo_product_page"
+								type="checkbox"
+								style=""
+								value="1"
+								class=""
+								placeholder=""
+								<?php if (get_option('ss_woo_product_page')) print 'checked'; ?>
+																/> <span class="description">Add media information to product page.</span>						</td>
+					</tr><tr valign="top">
+						<th scope="row" class="titledesc">
+							<label for="">Symbiostock Artist Network</label>
+													</th>
+						<td class="forminp forminp-number">
+							<span class="description"><a href="http://www.symbiostock.org/artist-network-submit/" target="_blank">Include your store</a> in the <a href="http://www.symbiostock.org/artist-network/" target="_blank"><img src="<?php print $GLOBALS['currSS']->ss_web_assets_dir; ?>ss_fullico.png" style='max-width:100px;vertical-align:bottom;padding-right:2px;padding-left:2px;'></a> Artist Network <b>(Recommended)</b></span>						</td>
+					</tr><tr valign="top">
+						<th scope="row" class="titledesc">
+							<label for="ss_pingback">Artist Network Inclusion</label>
+													</th>
+						<td class="forminp forminp-number">
+							<input
+								name="ss_pingback"
+								id="ss_pingback"
+								type="checkbox"
+								style=""
+								value="1"
+								class=""
+								placeholder=""
+								<?php if (get_option('ss_pingback')) print 'checked'; ?>
+																/> <span class="description">Permits the Symbiostock Artist Network to confirm that your site is running Symbiostock. <b>(Recommended)</b></span>						</td>
+					</tr><tr valign="top">
+						<th scope="row" class="titledesc">
+							<label for="ss_auto_publish">Auto-publish Products</label>
+													</th>
+						<td class="forminp forminp-number">
+							<input
+								name="ss_auto_publish"
+								id="ss_auto_publish"
+								type="checkbox"
+								style=""
+								value="1"
+								class=""
+								placeholder=""
+								<?php if (get_option('ss_auto_publish')) print 'checked'; ?>
+																/> <span class="description">Scheduler will auto-publish media with meta content. <b>(Recommended)</b></span>						</td>
+					</tr></table><br><h3>Watermark</h3>
+<img src="<?php print $GLOBALS['currSS']->ss_cron_loc; ?>&ss_wm=1" style='max-width:400px;max-height:250px;padding-top:10px;'>
+<table class="form-table"><tr valign="top">
+						<th scope="row" class="titledesc">
+							<label for="ss_watermarkpercent">Watermark to image size ratio (in percent)</label>
+													</th>
+						<td class="forminp forminp-number">
+							<input
+								name="ss_watermarkpercent"
+								id="ss_watermarkpercent"
+								type="number"
+								style=""
+								value="<?php print get_option('ss_watermarkpercent'); ?>"
+								class=""
+								placeholder=""
+																/> <span class="description">This is the size of the watermark that will be imposed upon each stock image preview. Min: 1, Max:100.</span>						</td>
+					</tr><tr valign="top">
+						<th scope="row" class="titledesc">
+							<label for="ss_watermarkpercent">Upload new watermark</label>
+													</th>
+						<td class="forminp forminp-number">
+							<input type="file" id="ss_new_watermark" name="ss_new_watermark"><br><span class="description">Replace the current watermark with a new one. Thumbnails will be regenerated systematically through the maintenance process. PNG format, 150kb max. Recommended opacity: 15-30%.</span>						</td>
+					</tr><tr valign="top">
+						<th scope="row" class="titledesc">
+							<label for="ss_download_expiry">Reset watermark</label>
+													</th>
+						<td class="forminp forminp-number">
+							<input
+								name="ss_reset_watermark"
+								id="ss_reset_watermark"
+								type="checkbox"
+								style=""
+								value="1"
+								class=""
+								placeholder=""
+																/> <span class="description">Resets the media watermark.</span>						</td>
+					</tr></table>
+
+<?php do_action('ss_end_options'); ?>
+
+		<p class="submit">
+							<input name="save" class="button-primary" type="submit" value="Save changes" /></p>
+	</form>
+</div>
+<?php
+	}
+}
+
+add_action('ss_end_options','ss_display_details');
+function ss_display_details() {
+?>
+<br><h3>FTP Upload directory</h3>
+<input type="text" value="<?php echo $GLOBALS['currSS']->ss_media_upload_dir; ?>" readonly size='100'/><br><br>
+Upload all media files here via <a href="https://filezilla-project.org/" target="_blank">FTP</a>. Files will be processed systematically and added into the system.<br><br>
+<br><h3>Maintenance command (cron job) <a href="<?php print $GLOBALS['currSS']->ss_cron_loc; ?>" target="_blank">(Run now)</a></h3>
+<input type="text" value="<?php echo 'curl --silent \''.$GLOBALS['currSS']->ss_cron_loc.'\' &>/dev/null'; ?>" readonly size='100'/><br><br>
+Run once a minute via operating system scheduler for all FTP processing and other maintenance actions.<br><br>
+<?php
+}
+
+?>
